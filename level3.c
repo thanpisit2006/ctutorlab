297b1d4690e5fe695888628cf820f9b8e8acf248
supakornphonphichai-beep <supakornphonphichai@gmail.com>
2025-12-10 23:29:09 +0700

    Update level3.c

--- level3.c
+++ level3.c
@@ -20,3 +20,31 @@ void play_level3(Player *player) {
         printf("Failed to update your score.\n");
     }
 }
+
+//🏝️ STORY: “Survival Decision System on a Deserted Island”
+
+/*You wake up on a deserted island after a shipwreck.
+You only have a backpack, a small knife, and a watch.
+To survive, you must make the right decisions in each situation!
+Let’s create a decision-making program using if–else to help you survive.*/
+
+/*🪶 Problem 1 (Easy): Check the Weather Before Exploring
+🧭 Problem Description*/
+
+/*Write a program to determine whether today is suitable for exploring the island,
+using the wind speed (in kilometers per hour) entered by the user.*/
+
+//🔹 Conditions
+
+//If the wind speed is less than 10, display "Go explore!" (safe to explore)
+
+//If the wind speed is between 10–24, display "Be careful!" (you may explore but should be cautious)
+
+//If the wind speed is 25 or more, display "Stay in camp!" (dangerous to go out)
+
+/*🔹 Examples
+Input	Output
+5	Go explore!
+15	Be careful!
+30	Stay in camp!8*/
+
